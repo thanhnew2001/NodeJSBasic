8d6b514e5b90a24d1d4bae30170293afb8e39717
thanhnew2001 <thanh.hispvietnam@gmail.com>
2021-09-08 09:03:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.get("/welcome", auth, (req, res) => {
 
 
 // All other GET requests not handled before will return our React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-// });
+ app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+ });
 
 module.exports = app;
